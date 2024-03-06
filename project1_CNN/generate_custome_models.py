342d7a58b33bfdc12813f6f44f1227e80a99da48
jakub-piw <72337603+jakub-piw@users.noreply.github.com>
2024-03-06 17:10:21 +0100

    First attempts of training and saving

--- project1_CNN/generate_custome_models.py
+++ project1_CNN/generate_custome_models.py
@@ -41,7 +41,7 @@ def generate_model1(input_shape,
         LeakyReLU(0.1),
         Dropout(dropout_rate2),
         Dense(num_classes, activation='softmax')
-        ], name='Custom model 1')
+        ], name='Custom_model_1')
 
     lr_schedule = ExponentialDecay(
         initial_learning_rate=learning_rate,
@@ -59,67 +59,6 @@ def generate_model1(input_shape,
 
 
 def generate_model2(input_shape, 
-                    num_classes, 
-                    learning_rate=0.001, 
-                    dropout_conv=0.3, 
-                    dropout_dense=0.4, 
-                    weight_decay=0.0001, 
-                    decay_steps=100000, 
-                    decay_rate=0.96):
-    """
-    Generate Keras Sequential model according to proposed architecture (2).
-
-    Args:
-        input_shape (tuple): The shape of the input data.
-        num_classes (int): Number of classes in the target output.
-        learning_rate (float, optional): Initial learning rate of the neural network. Defaults to 0.001.
-        dropout_conv (float, optional): Dropout rate for convolutional layers. Defaults to 0.3.
-        dropout_dense (float, optional): Dropout rate for dense layers. Defaults to 0.4.
-        weight_decay (float, optional): L2 regularization factor. Defaults to 0.0001.
-        decay_steps (int, optional): Number of steps for the learning rate decay. Defaults to 100000.
-        decay_rate (float, optional): Rate of decay for the learning rate. Defaults to 0.96.
-
-    Returns:
-        Keras Sequential model: Compiled and ready to use.
-    """
-
-    model = Sequential(name='Custom model 2')
-
-    # Adding Conv2D, MaxPooling2D, Dropout, BatchNormalization, and Dense layers similar to the previous function
-    model.add(Conv2D(filters=32, kernel_size=(3,3), input_shape=input_shape, activation='relu', padding='same', kernel_initializer='he_uniform', kernel_regularizer=l2(weight_decay)))
-    model.add(Conv2D(filters=32, kernel_size=(3,3), activation='relu', padding='same', kernel_initializer='he_uniform', kernel_regularizer=l2(weight_decay)))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(dropout_conv))
-    model.add(Conv2D(filters=64, kernel_size=(3,3), activation='relu', padding='same', kernel_initializer='he_uniform', kernel_regularizer=l2(weight_decay)))
-    model.add(Conv2D(filters=64, kernel_size=(3,3), activation='relu', padding='same', kernel_initializer='he_uniform', kernel_regularizer=l2(weight_decay)))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(dropout_conv))
-    model.add(Conv2D(filters=128, kernel_size=(3,3), activation='relu', padding='same', kernel_initializer='he_uniform', kernel_regularizer=l2(weight_decay)))
-    model.add(Conv2D(filters=128, kernel_size=(3,3), activation='relu', padding='same', kernel_initializer='he_uniform', kernel_regularizer=l2(weight_decay)))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(dropout_conv))
-    model.add(BatchNormalization())
-    model.add(Flatten())
-    model.add(Dense(256, activation='relu', kernel_initializer='he_uniform', kernel_regularizer=l2(weight_decay)))
-    model.add(Dropout(dropout_dense))
-    model.add(Dense(num_classes, activation='softmax'))
-
-    # Learning rate scheduling
-    lr_schedule = ExponentialDecay(
-        initial_learning_rate=learning_rate,
-        decay_steps=decay_steps,
-        decay_rate=decay_rate,
-        staircase=True)
-
-    # Compile the model
-    model.compile(loss='categorical_crossentropy',
-                  optimizer=Adam(learning_rate=lr_schedule), 
-                  metrics=['accuracy'])
-
-    return model
-
-
-def generate_model3(input_shape, 
                     num_classes, 
                     learning_rate=0.001,
                     dropout_rate1=0.2,
@@ -157,7 +96,7 @@ def generate_model3(input_shape,
         Dense(32, activation='relu'),
         Dropout(dropout_rate3),
         Dense(num_classes, activation='softmax')
-        ], name='Custom model 3')
+        ], name='Custom_model_2')
 
     lr_schedule = ExponentialDecay(
         initial_learning_rate=learning_rate,
